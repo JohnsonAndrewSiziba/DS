b6126043dcfeb44934f89aa11126126ff23cddc1
Johnson Andrew Siziba <siziba.uz@outlook.com>
2022-01-28 19:25:21 +0200

    wk

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::name('website.')->group(function () {
+    Route::get('/', [App\Http\Controllers\Website\WebsiteController::class, 'home'])->name('home');
+
 });
